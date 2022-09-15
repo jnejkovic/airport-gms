40856d625115cb5163cf93bc6b655dd4567e939b
jnejkovic <jnejkovic@gmail.com>
2022-09-15 20:30:35 +0200

    sql file formated

--- src/main/resources/db/migration/V1__initial_schema_migration.sql
+++ src/main/resources/db/migration/V1__initial_schema_migration.sql
@@ -1,53 +1,48 @@
-    drop table if exists airport;
-
-    drop table if exists flight;
-
-    drop table if exists gate;
-
-    drop table if exists hibernate_sequence;
-
-    create table airport (
-       airport_id integer not null,
+create table airport (
+   airport_id integer not null,
         airport_name varchar(255) not null,
         version integer,
         primary key (airport_id)
-    ) engine=InnoDB;
+    ) engine = InnoDB;
 
-    create table flight (
+create table flight (
        flight_id integer not null,
         flight_index varchar(255) not null,
         version integer,
         gate integer,
         primary key (flight_id)
-    ) engine=InnoDB;
+    ) engine = InnoDB;
 
-    create table gate (
+create table gate (
        gate_id integer not null,
         gate_name varchar(3) not null,
         status varchar(255) not null,
         version integer,
         airport integer,
         primary key (gate_id)
-    ) engine=InnoDB;
+    ) engine = InnoDB;
 
-    create table hibernate_sequence (
+create table hibernate_sequence (
        next_val bigint
-    ) engine=InnoDB;
+    ) engine = InnoDB;
 
-    insert into hibernate_sequence values ( 1 );
+insert
+	into
+	hibernate_sequence
+values (1);
 
-    alter table airport
+alter table airport
        add constraint UK_airport_name unique (airport_name);
 
-    alter table flight
+alter table flight
        add constraint UK_flight_index unique (flight_index);
 
-    alter table flight
+alter table flight
        add constraint FK7_gate_id
        foreign key (gate)
        references gate (gate_id);
 
-    alter table gate
+alter table gate
        add constraint FK_airport_id
        foreign key (airport)
        references airport (airport_id);
